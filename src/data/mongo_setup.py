e5a9fcdbe70608c35cb9174cb28c8ce797c673f1
NiteshBabu <niteshbabusharma@gmail.com>
2020-09-16 10:51:23 +0530

    host keys updated

--- src/data/mongo_setup.py
+++ src/data/mongo_setup.py
@@ -1,15 +1,15 @@
 from mongoengine import connect
 import os, pprint
-
 from keys import host 
+
 # import pymongo
-# client = pymongo.MongoClient("mongodb://self:nitesh123@cluster0-shard-00-00.63hur.mongodb.net:27017,cluster0-shard-00-01.63hur.mongodb.net:27017,cluster0-shard-00-02.63hur.mongodb.net:27017/sample_mflix?ssl=true&replicaSet=atlas-s2h4ty-shard-0&authSource=admin&retryWrites=true&w=majority")
+# client = pymongo.MongoClient("mongodb://user:password@cluster0-shard-00-00.63hur.mongodb.net:27017,cluster0-shard-00-01.63hur.mongodb.net:27017,cluster0-shard-00-02.63hur.mongodb.net:27017/sample_mflix?ssl=true&replicaSet=atlas-s2h4ty-shard-0&authSource=admin&retryWrites=true&w=majority")
 
 # alias_core = 'core'
 # db = 'Bird_Motel'
 # data = dict( 
-#   username = 'self', 
-#   password = 'nitesh123', 
+#   username = '', 
+#   password = '', 
 #   host = 'cluster0-shard-00-00.63hur.mongodb.net',
 #   port = '27017', 
 #   authentication_source = 'admin',
